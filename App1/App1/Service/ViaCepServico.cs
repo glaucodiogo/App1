320ec2afcf6134b99e4172db5d29afe58892d9af
Glauco Diogo <glaucodiogoalves@gmail.com>
2018-11-10 23:21:21 -0200

    Comunicao com web servce ,completada

---
+++ App1/App1/Service/ViaCepServico.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App1.Service.Model;
+using Newtonsoft.Json;
+
+namespace App1.Service
+{
+    public class ViaCepServico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+            string conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+
+        }
+    }
+}
